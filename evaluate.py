7baf8f308062991b93e2f5dfc7639935f15ae941
Pradyumn R Pai <pradyumn3.1415@gmail.com>
2026-03-08 19:38:19 +0530

    Add SSIM, PSNR

--- evaluate.py
+++ evaluate.py
@@ -7,15 +7,15 @@ from constants.TrainingParameters import *
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def evaluateModel(image):
-    lf, hf = splitTensor(lf, hf)
+
+if __name__=='__main__':
+    test_dataset = ImageDataset(input_dir=TEST_DATA_PATH+'/input', gt_dir=TEST_DATA_PATH+'/GT')
+
     model = ImageEnhancementNetwork()
     model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
     model.to(DEVICE)
     model.eval()
 
-if __name__=='__main__':
-    test_dataset = ImageDataset(input_dir=TEST_DATA_PATH+'/input',gt_dir=TEST_DATA_PATH+'/GT', limitImages=10)
-    eval = Evaluator(test_dataset)
+    eval = Evaluator(test_dataset, model, DEVICE)
 
     print(eval.evaluate())
--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,22 +1,36 @@
 from torch.utils.data import Dataset, DataLoader
+from torchmetrics.functional.image import peak_signal_noise_ratio as psnr
+from pytorch_msssim import ssim
 from .uiqm import UIQM
 
 class Evaluator:
-    def __init__(self, data: Dataset):
+    def __init__(self, data: Dataset, model, device):
         self.data = data
         self.loader = DataLoader(data, batch_size=1)
-        self.metrics = {
-            "UIQM": UIQM.uiqm #Add other metrics here
+        self.model = model
+        self.device = device
+        self.metrics = { #Add other metrics here
+            "UIQM": UIQM.uiqm,
+            "SSIM": lambda x,y: float(ssim(x, y, data_range=1.0, size_average=True)),
+            "PSNR": lambda x,y: float(psnr(x, y, data_range=1.0))
         }
     
     def average_metric(self, metricName: str) -> float:
         metric = self.metrics[metricName]
-        num = 1
+        num = len(self.data)
         assert num>0
         total = 0.0
+        num_done = 0
         for lf, hf, gt_lf, gt_hf in self.loader:
-            total += metric(lf+hf, gt_lf+gt_hf)
-        return total/num
+            lf, hf, gt_lf, gt_hf = lf.to(self.device), hf.to(self.device), gt_lf.to(self.device), gt_hf.to(self.device)
+            total += metric(self.model(lf, hf), gt_lf+gt_hf)
+            num_done += 1
+            if num_done%20==0:
+                print(f"Calculating {metricName}:{num_done}/{num}")
+
+        res =  total/num
+        print(f"Result: {metricName}={res}\n")
+        return res
     
     def evaluate(self) -> dict:
         result = {}

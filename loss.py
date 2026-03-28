2e2ef2f0e19c66f85d0a6d873801e8eb33ac90d4
hari <nhari142004@gmail.com>
2026-03-28 21:49:25 +0530

    Updated the loss functions
    1. Removed UIQM as a loss only keeping it as a metric
    2. Added local_contrast_loss
    3. Added lab_color_loss

--- loss.py
+++ loss.py
@@ -6,8 +6,8 @@ import torchvision.models as models
 
 from model import ImageEnhancementNetwork
 from constants.TrainingParameters import *
-from metrics.uiqm import UIQM
 
+import kornia.color as KC
 
 class PerceptualLoss(nn.Module):
     def __init__(self):
@@ -26,16 +26,19 @@ class PerceptualLoss(nn.Module):
 
 
 class CompositeLoss(nn.Module):
-    def __init__(self, model: ImageEnhancementNetwork, lambda_mae=100, lambda_ssim=40, lambda_hf=60, lambda_lf=60, lambda_perceptual=2, lambda_uiqm=60):
+    def __init__(self, model: ImageEnhancementNetwork, lambda_mae=100, lambda_ssim=40, lambda_hf=60, lambda_lf=60, lambda_perceptual=2, lambda_color=60, lambda_contrast=40):
         super(CompositeLoss, self).__init__()
         self.lambda_mae = lambda_mae
         self.lambda_ssim = lambda_ssim
         self.lambda_hf = lambda_hf
         self.lambda_lf = lambda_lf
-        self.lambda_uiqm = lambda_uiqm
+        
         self.lambda_perceptual = lambda_perceptual
         self.model = model
         
+        self.lambda_color = lambda_color
+        self.lambda_contrast = lambda_contrast
+
         self.mae_refinement = nn.L1Loss()
         self.mse_prelim = nn.MSELoss()
         self.l1_grad = nn.L1Loss()
@@ -46,9 +49,31 @@ class CompositeLoss(nn.Module):
     def ssim_loss(self, output, gt):
         return 1 - ssim(output, gt, data_range=1.0, size_average=True)
     
-    def uiqm_loss(self, image):
-        return -UIQM.uiqm(image)
+    def lab_color_loss(self, output, gt):
+        lab_out = KC.rgb_to_lab(output.float())
+        lab_gt= KC.rgb_to_lab(gt.float())
 
+        ab_out = lab_out[:, 1:, :, :]     
+        ab_gt  = lab_gt[:,  1:, :, :]
+
+        return F.l1_loss(ab_out, ab_gt, reduction='mean')
+
+
+    def local_contrast_loss(self, output, gt):
+        gray_out = output.mean(dim=1, keepdim=True)
+        gray_gt  = gt.mean(dim=1, keepdim=True)
+
+        def local_std(gray):
+            mean   = F.avg_pool2d(gray, kernel_size=8, stride=1, padding=3)
+            mean_sq = F.avg_pool2d(gray*gray, kernel_size=8, stride=1, padding=3)
+            var    = mean_sq - mean * mean
+            std    = torch.sqrt(var.clamp(min=1e-8))
+            return std
+
+        std_out = local_std(gray_out)
+        std_gt  = local_std(gray_gt)
+
+        return F.l1_loss(std_out, std_gt, reduction='mean')
     
     def forward(self, lf, hf, gt_lf, gt_hf):
         gt = gt_hf + gt_lf
@@ -57,13 +82,14 @@ class CompositeLoss(nn.Module):
         l_mae_refinement = self.mae_refinement(output, gt)
         l_ssim = self.ssim_loss(output, gt)
         l_perceptual = torch.log1p(self.perceptual(output, gt))
-        l_uiqm = self.uiqm_loss(output)
+        
+        l_color = self.lab_color_loss(output, gt)
+        l_contrast = self.local_contrast_loss(output, gt)
         
         l_mse_prelim = self.mse_prelim(lf_output,gt_lf)
-
         l_mae_prelim = self.l1_prelim(hf_output, gt_hf)
         
-        l_refinement = self.lambda_mae * l_mae_refinement + self.lambda_ssim * l_ssim + self.lambda_perceptual*l_perceptual + l_uiqm*self.lambda_uiqm
+        l_refinement = self.lambda_mae * l_mae_refinement + self.lambda_ssim * l_ssim + self.lambda_perceptual*l_perceptual + self.lambda_color*l_color + self.lambda_contrast*l_contrast
 
         l_prelim = self.lambda_lf*l_mse_prelim + self.lambda_hf*l_mae_prelim
 
